a27a287cdb3c6735bb5a9ef38f0e6323d9d8f694
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2023-11-09 11:12:31 +0700

    finish

---
+++ app/src/main/java/com/example/estehjumbo/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.estehjumbo.data
+
+import com.example.estehjumbo.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche,
+    )
+}
